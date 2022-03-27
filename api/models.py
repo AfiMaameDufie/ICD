defa35b5d907dbfbfd51075870f567f3a60ca996
Abigail Afi Gbadago <afimaamedufie@gmail.com>
2022-03-27 20:43:06 +0000

    Create model Diagnosis

--- api/models.py
+++ api/models.py
@@ -1,3 +1,4 @@
+from unicodedata import category
 from django.db import models
 
 class DiagnosisCategory(models.Model):
@@ -20,3 +21,24 @@ class DiagnosisCategory(models.Model):
     def __str__(self):
         return self.code
 
+
+class Diagnosis(models.Model):
+    category = models.ForeignKey(to=DiagnosisCategory, related_name='diagnosis', on_delete=models.CASCADE)
+    diagnosis_code = models.CharField(max_length=10, default='', blank=True)
+    full_code = models.CharField(max_length=20)
+    abbreviation_description = models.CharField(max_length=500)
+    full_description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('full_code',)
+    
+    def __str__(self):
+        return self.full_code
+    
+    # override save method to auto populate self.full_code field
+    def save(self,*args,**kwargs):
+        self.full_code = self.category.code + self.diagnosis_code
+        super(Diagnosis, self).save(*args, **kwargs)
+    
